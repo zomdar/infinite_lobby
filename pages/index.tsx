0dd932ccc887d87e055f7b3692f652f5e24f3dbc
zomdar <zomdar@gmail.com>
2022-12-11 18:14:24 -0500

    updating home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Scroller from "./components/Scroller";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {}, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,19 +15,19 @@ export default function Home() {
         <meta name="description" content="infinite scroll" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <div className={styles.scrollContainer}>
-          <div className={styles.sliding_background}></div>
-          {/* <span className={styles.logo}>
-            <Image
-              className={styles.sliding_background}
-              src="/welcome_name.svg"
-              alt="Vercel Logo"
-              width={5076}
-              height={100}
-            />
-          </span> */}
+        <div className={styles.hero_section}>
+          <div className={styles.scrollContainer}>
+            <Scroller />
+            <div className={styles.textContainer}>
+              <p className={styles.transducer_test}>
+                WELCOME TO HAKAJI! MEET OUR COLLECTION OF 3.333 UNIQUE GIRAFFE
+                SLAM. SUPPORT AND JOIN OUR COMMUNITY, A NEW WORLD ARE WAITING
+                FOR YOU.
+              </p>
+            </div>
+          </div>
+          <div className={styles.scrollContainer}></div>
         </div>
       </main>
     </div>
