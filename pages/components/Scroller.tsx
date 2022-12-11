701850c5c88c156967cd1e7f1e2cc69375750fd7
zomdar <zomdar@gmail.com>
2022-12-11 18:14:01 -0500

    adding scroller component

---
+++ pages/components/Scroller.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styles from "../../styles/Scroller.module.css";
+
+const Scroller = () => {
+  return <div className={styles.sliding_background}></div>;
+};
+
+export default Scroller;
